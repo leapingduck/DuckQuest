d06b4cb785518d41aec2a142e06c2644d1dc2a49
Dylan <leapingduck88@gmail.com>
2023-03-29 17:11:46 -0500

    added a couple txts

--- DuckQuest/Utilities.cs
+++ DuckQuest/Utilities.cs
@@ -11,21 +11,43 @@ namespace DuckQuest
 
         public Utilities() { }
 
-        internal static void ItemTest()
+        internal static void WeaponTest()
         {
-            string path = @"C:\Users\dmoore\source\repos\DuckQuest\DuckQuest\Data\items.txt";
+            string path = @"C:\Users\dmoore\source\repos\DuckQuest\DuckQuest\Data\weapons.txt";
             if (File.Exists(path))
             {
-                string[] itemsAsString = File.ReadAllLines(path);
-                for (int i = 0; i < itemsAsString.Length; i++)
+                string[] weaponAsString = File.ReadAllLines(path);
+                for (int i = 0; i < weaponAsString.Length; i++)
                 {
-                    string[] itemSplits = itemsAsString[i].Split(',');
-                    string itemName = itemSplits[0];
-                    string itemDescription = itemSplits[1];
-                    string itemAttribute = itemSplits[2];
-                    string itemDamage = itemSplits[3];
-                    string itemToHit = itemSplits[4];
-                    Console.WriteLine($"The {itemName} is {itemDescription}. It uses {itemAttribute} and does {itemDamage} damage with a +{itemToHit} to hit");
+                    string[] weaponSplits = weaponAsString[i].Split(',');
+                    string weaponName = weaponSplits[0];
+                    string weaponDescription = weaponSplits[1];
+                    string weaponAttribute = weaponSplits[2];
+                    string weaponDamage = weaponSplits[3];
+                    string weaponToHit = weaponSplits[4];
+                    Console.WriteLine($"The {weaponName} is {weaponDescription}. It uses {weaponAttribute} and does {weaponDamage} damage with a +{weaponToHit} to hit");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+
+        internal static void ArmorTest()
+        {
+            string path = @"C:\Users\dmoore\source\repos\DuckQuest\DuckQuest\Data\armor.txt";
+            if (File.Exists(path))
+            {
+                string[] armorAsString = File.ReadAllLines(path);
+                for (int i = 0; i < armorAsString.Length; i++)
+                {
+                    string[] armorSplits = armorAsString[i].Split(',');
+                    string armorName = armorSplits[0];
+                    string armorDescription = armorSplits[1];
+                    string armorAttribute = armorSplits[2];
+                    string armorDamage = armorSplits[3];
+                    string armorPenalty = armorSplits[4];
+                    Console.WriteLine($"The {armorName} is {armorDescription}. It uses {armorAttribute} and reduces {armorDamage} damage. It does reduce your speed by {armorPenalty}");
                 }
             }
 
